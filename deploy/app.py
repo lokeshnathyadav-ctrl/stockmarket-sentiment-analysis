555b2a4477818144b6bd1c396fc782dabb73858e
lokeshnathyadav-ctrl <lokeshnath.yadav@gmail.com>
2026-05-31 03:25:20 +0530

    deploy issues #9

--- deploy/app.py
+++ deploy/app.py
@@ -3,8 +3,10 @@ import streamlit as st
 import pandas as pd
 from huggingface_hub import hf_hub_download
 import joblib
-
-model_path = hf_hub_download(repo_id="Lokeshnathy/Stock-market-news-Analyzer",filename="stk_mrkt_ns_anlzr_v1.joblib")
+import torch
+#from transformers import AutoModelForYourTask
+#model = AutoModelForYourTask.from_pretrained('Stock-market-news-Analyzer
+model_path = hf_hub_download(repo_id="Lokeshnathy/Stock-market-news-Analyzer",filename="best_model_for_stock_news_analyze_v1.joblib")       
 model = joblib.load(model_path)
 
 st.title("Stock Market News - Sentiment Finder")
